4a574f07621d2e17457253d3ad7483f6c0a72a49
R.W.Majeed <raphael@majeed.de>
2016-10-05 11:31:27 +0200

    easy testing with preferences

--- src/test/java/org/aktin/dwh/prefs/impl/TestPreferences.java
+++ src/test/java/org/aktin/dwh/prefs/impl/TestPreferences.java
@@ -2,8 +2,8 @@ package org.aktin.dwh.prefs.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.SQLException;
 
+import org.aktin.Preferences;
 import org.aktin.dwh.PreferenceKey;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,17 +13,27 @@ public class TestPreferences {
 	private PropertyFilePreferences prefs;
 	
 	@Before
-	public void loadTestPrefs() throws SQLException, IOException{
+	public void loadTestPrefs() throws IOException{
 		try( InputStream in = getClass().getResourceAsStream("/aktin.properties") ){
 			prefs = new PropertyFilePreferences(in);
 		}
 	}
 	
 	@Test
-	public void verifyMandatoryPreferenceKeys() throws SQLException, IOException{
+	public void verifyMandatoryPreferenceKeys() throws IOException{
 		for( PreferenceKey pref : PreferenceKey.values() ){
 			String value = prefs.get(pref.key());
 			Assert.assertNotNull("Preference entry expected for key "+pref.key(), value);
 		}
+	}
+
+	public Preferences getPreferences(){
+		return prefs;
+	}
+
+	public static Preferences getTestPreferences() throws IOException{
+		TestPreferences p = new TestPreferences();
+		p.loadTestPrefs();
+		return p.getPreferences();
 	}
 }
